99124841fb72b8946957e816cc5a87551cc68861
Victor Gutiérrez <victor.hg@icloud.com>
2018-07-09 17:04:20 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ for file in *.xml
 do
 	echo "Procesando: $file..."
 	output=$(echo $file | sed s/\.xml$//)
-  python Nessus2Excel.py "$file" "$output.xlsx" "medium"
+	python Nessus2Excel.py "$file" "$output.xlsx" "medium"
 done
